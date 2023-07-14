223797b8b21ce6a29831c72c054b761f93fa9a20
Henrique Rodrigues <50811345+henriqurr@users.noreply.github.com>
2023-07-13 21:34:01 -0300

    First commit

---
+++ valorantmarket/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace valorantmarket.Models
+{
+    public class User
+    {
+        public ulong ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
